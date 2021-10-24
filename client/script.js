0cd4701c502f7a98901337741c9fe1648eda21f4
Vaibhav <singhvaibhav557hr@gmail.com>
2021-10-24 17:16:24 +0530

    Polling

--- client/script.js
+++ client/script.js
@@ -1,19 +1,41 @@
 var socket = io();
-const roomName=document.getElementById('roomName')
-const uname=document.getElementById('identity')
-const password=document.getElementById('pass')
-
-socket.on(' connect', () => {
-   console.log('Connected', socket.id);
-})
-function checkPass() {
-   // const roomName=document.getElementById('roomName')
-   // const uname=document.getElementById('identity')
-   // const password = document.getElementById('pass')
-   socket.emit('tryFill',{ room: roomName, username: uname, password: password})
-      socket.emit('joinRoom', { room: roomName, username: uname, password: password});
-      socket.on('passwordFeedback', function (letter,roomInf) {
-       console.log(roomInf);
-         roomInfo.innerHTML = ` <li>You have entered ${letter} password</li> <li></li>`
-      })
-   }
+// const roomName=document.getElementById('roomName')
+// const uname=document.getElementById('identity')
+// const password=document.getElementById('pass')
+const showButtons = document.getElementById("btn");
+const roomInfo = document.getElementById("roomInfo");
+const roomName = document.getElementById("roomName").value;
+socket.on(" connect", () => {
+  console.log("Connected", socket.id);
+});
+function joinRoom() {
+  socket.emit("roomTojoin", roomName);
+}
+socket.on("roomJoined", function (roName) {
+  roomInfo.innerHTML = "You are now in " + roName;
+  showButtons.style.display = "block";
+});
+function vote1() {
+  socket.emit("vote1",roomName);
+  socket.on("vote1done", function (vote1Value) {
+    document.getElementById("vote1Value").innerHTML = vote1Value;
+  });
+}
+function vote2() {
+  socket.emit("vote2",roomName);
+  socket.on("vote2done", function (vote2Value) {
+    document.getElementById("vote2Value").innerHTML = vote2Value;
+  });
+}
+function vote3() {
+  socket.emit("vote3",roomName);
+  socket.on("vote3done", function (vote3Value) {
+    document.getElementById("vote3Value").innerHTML = vote3Value;
+  });
+}
+function vote4() {
+  socket.emit("vote4",roomName);
+  socket.on("vote4done", function (vote4Value) {
+    document.getElementById("vote4Value").innerHTML = vote4Value;
+  });
+}
--- socketStuff.js
+++ socketStuff.js
@@ -4,38 +4,30 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 let rooms={}
 app.use(express.static('client'))
+let vote=[0,0,0,0]
 
 io.on('connection', function (socket) {
-    socket.on("joinRoom", ({ username, room, password }) => {
-        let isEntryAvailable = userJoin(socket.id, username, room, password);
-        if (isEntryAvailable) {
-            socket.emit('passwordFeedback', 'correct',rooms)
-        } else {
-            socket.emit('passwordFeedback', 'wrong',rooms)
-        }
+    socket.on('roomTojoin', function (rName) {
+       console.log(rName);
+        socket.join(rName)
+        socket.emit('roomJoined',rName)
     })
-    function userJoin (socketId, username, room, password) {
-        if (!rooms[room] || (rooms[room] && !rooms[room].password)) {
-            if (!rooms[room]) {
-                rooms[room] = {};
-            }
-            rooms[room].password = password;
-            rooms[room][socketId] = username;
-            rooms[socketId] = room;
-            return true;
-        } else {
-            if (rooms[room].password == password) {
-                rooms[socketId] = room;
-                rooms[room][socketId] = username;
-                socket.join(room);
-                return true;
-            } else {
-                return false;
-            }
-        }
-    }
-
-
+    socket.on('vote1', function (roomForVote) {
+        vote[0] = vote[0] + 1
+        socket.to(roomForVote).emit('vote1done',vote[0])
+    })
+    socket.on('vote2', function () {
+        vote[1] = vote[1] + 1
+        socket.to(roomForVote).emit('vote2done',vote[1])
+    })
+    socket.on('vote3', function () {
+        vote[2] = vote[2] + 1
+        socket.to(roomForVote).emit('vote3done',vote[2])
+    })
+    socket.on('vote4', function () {
+        vote[3] = vote[3] + 1
+        socket.to(roomForVote).emit('vote4done',vote[3])
+    })
    }) 
 
 http.listen(3000, function(){
