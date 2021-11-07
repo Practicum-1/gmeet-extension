9b4697451158e2ba3db8432e96ae4ffa81b99bb5
ShivanshVerma-coder <Shivansh29.sv@gmail.com>
2021-11-08 01:09:46 +0530

    fixes

--- client/script.js
+++ client/script.js
@@ -1,30 +1,32 @@
 var socket = io();
-let roomName = prompt("Enter name of room")
-document.getElementById('btn').addEventListener("click", submitMessage)
-document.getElementById('optionOne').addEventListener('click', countVoteOne)
+let roomName = prompt("Enter name of room");
+document.getElementById("btn").addEventListener("click", submitMessage);
+document.getElementById("optionOne").addEventListener("click", countVoteOne);
 
-document.getElementById('roomInfo').innerHTML=`You are now in ${roomName}  room  <br>`
-socket.on('connect', function () {
-    console.log("You are now connected  ");
-})
+document.getElementById(
+  "roomInfo"
+).innerHTML = `You are now in ${roomName}  room  <br>`;
+socket.on("connect", function () {
+  console.log("You are now connected  ");
+});
 
-function submitMessage(){
-    let msgText= document.getElementById('msg').value
-    socket.emit('joinRoom', roomName, msgText)
-
+function submitMessage() {
+  let msgText = document.getElementById("msg").value;
+  socket.emit("joinRoom", roomName, msgText);
 }
 
 function countVoteOne() {
-    socket.emit('countOne',roomName)
+  socket.emit("countOne", roomName);
 }
 
-socket.on('voteOneCounted', function (vote) {
-    console.log("one is pressed "+vote);
-    document.getElementById('voteOneValue').innerHTML= `Button is pressed ${vote} times`
-})
+socket.on("voteOneCounted", function (vote) {
+  console.log("one is pressed " + vote);
+  document.getElementById(
+    "voteOneValue"
+  ).innerHTML = `Button is pressed ${vote} times`;
+});
 
-
-socket.on('showMessage', function (message) {
-    console.log(message);
-    document.getElementById('msgText').innerHTML=`<lli> ${message} </li>`
-})
+socket.on("showMessage", function (message) {
+  console.log(message);
+  document.getElementById("msgText").innerHTML = `<li> ${message} </li>`;
+});
--- socketStuff.js
+++ socketStuff.js
@@ -1,36 +1,39 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-app.use(express.static('client'))
+var http = require("http").Server(app);
+var io = require("socket.io")(http);
+app.use(express.static("client"));
 let vote = 0;
-let voterDetail=[]
-let rooms = []
+let voterDetail = [];
+let rooms = {};
 
+io.on("connection", function (socket) {
+  console.log("Your id is " + socket.id);
+  socket.on("joinRoom", function (roomName, user) {
+    // console.log(roomName, user);
+    socket.join(roomName);
+    if (!rooms[roomName]) {
+      rooms[roomName] = [user];
+    } else {
+      rooms[roomName].push(user);
+    }
+    console.log(rooms);
+    io.to(roomName).emit("roomInfo", rooms[roomName]);
+  });
 
+  socket.on("countOne", function (rName) {
+    socket.join(rName);
+    var voter = { room: rName, vote: 0 };
+    voterDetail.push(voter);
+    console.log(voterDetail);
+    let obj = voterDetail.find((o) => o.room === rName);
+    console.log(obj.room);
+    obj.vote = obj.vote + 1;
+    console.log(` ${obj.room} has ${obj.vote} votes`);
+    io.to(rName).emit("voteOneCounted", obj.vote);
+  });
+});
 
-io.on('connection', function (socket) {
-console.log("Your id is "+ socket.id);
-    socket.on('joinRoom', function (roomName,msg) {
-        socket.join(roomName)
-        console.log(msg);
-        io.to(roomName).emit('showMessage',msg)
-    })
-    
-    
-    socket.on('countOne', function (rName) {
-        socket.join(rName)
-        var voter = { room: rName, vote: 0 }
-        voterDetail.push(voter)
-        console.log(voterDetail);
-        let obj = voterDetail.find(o => o.room === rName);
-console.log(obj.room);
-        obj.vote = obj.vote + 1
-        console.log(` ${obj.room} has ${obj.vote} votes`);
-        io.to(rName).emit('voteOneCounted',obj.vote)
-    })
-})
-
-http.listen(3000, function(){
-    console.log('listening on localhost:3000');
- });
+http.listen(3000, function () {
+  console.log("listening on localhost:3000");
+});
