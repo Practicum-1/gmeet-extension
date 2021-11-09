737c1c86bf493681e82fc899cde8d7798e5a0fa9
Vaibhav <singhvaibhav557hr@gmail.com>
2021-11-09 23:48:45 +0530

    voting

--- socketStuff.js
+++ socketStuff.js
@@ -23,7 +23,9 @@ io.on("connection", function (socket) {
         displayName: user,
       });
     }
-    console.log(JSON.stringify(rooms));
+    // console.log(JSON.stringify(rooms));
+    console.log(rooms[roomName])
+    // console.log(rooms[roomName]['participants'])
     io.to(roomName).emit("roomInfo", rooms[roomName]);
   });
 
@@ -38,6 +40,54 @@ io.on("connection", function (socket) {
     console.log(` ${obj.room} has ${obj.vote} votes`);
     io.to(rName).emit("voteOneCounted", obj.vote);
   });
+// event 1 will check if a poll with a given index already exist or not 
+  socket.on("event1", function (pollIndex) {
+    
+    if (!rooms[roomName]) {
+      polls = [{
+        index: pollIndex,
+        textString: "question",
+        options: [],
+        createdBy: socket.id,
+        createdAt: Date.now(),}]
+        room[roomName]={polls,}
+    }
+    else {
+      rooms[roomName]["polls"].push({
+        index: pollIndex,
+        textString: "polling question",
+        options: [],
+        createdBy: socket.id,
+        createdAt: Date.now(),
+        
+      })
+    }
+    rooms[roomName]['polls'][pollIndex]['options'].push({
+      option: 'option A',
+      votes:[]
+    })
+    rooms[roomName]['polls'][pollIndex]['options'].push({
+      option: 'option B',
+      votes:[]
+    })
+    rooms[roomName]['polls'][pollIndex]['options'].push({
+      option: 'option C',
+      votes:[]
+    })
+    rooms[roomName]['polls'][pollIndex]['options'].push({
+      option: 'option D',
+      votes:[]
+    })
+    
+  })
+  
+  // event 2 will pass the socket id to votes key of the respective option of the respective poll
+  socket.on("event2", function (pollIndex, optionIndex) {
+    
+    rooms[roomName]['polls'][pollIndex]['options'][optionIndex]['votes'].push(socket.id);
+
+
+  })
 
   socket.on("disconnect", function () {
     console.log("disconnected");
